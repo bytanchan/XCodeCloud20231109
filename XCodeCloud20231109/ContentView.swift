f77bc99c6ed88ac55be10df9b45870b4bde1bb17
bytanchan <134441027+bytanchan@users.noreply.github.com>
2023-11-09 23:08:01 -0600

    tyga

--- XCodeCloud20231109/ContentView.swift
+++ XCodeCloud20231109/ContentView.swift
@@ -8,78 +8,42 @@
 import SwiftUI
 import CoreData
 
+import SwiftUI
+
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State var total = ""
+    @State var tipPercent = 15.0
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        VStack {
+            Image("tyga")
+                .resizable() // Makes the image resizable
+                .scaledToFit() // Scales the image to fit within the frame preserving aspect ratio
+                .frame(width: 100, height: 100) // Sets the frame to specific width and height
+            HStack {
+                Image(systemName: "dollarsign.circle.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                    .font(.title)
+                Text("Tip Calculator")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
+            HStack {
+                Text("$")
+                TextField("Amounts", text: $total)
             }
-            Text("Select items")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            HStack {
+                Slider(value: $tipPercent, in: 1...30, step: 1.0)
+                Text("\(Int(tipPercent))")
+                Text("%")
+            }
+            if let totalNum = Double(total) {
+                Text("Tip Amount: $\(totalNum * tipPercent * 0.01, specifier: "%0.2f")")
             }
         }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        .padding()
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
